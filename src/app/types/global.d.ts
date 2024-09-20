9a7705780f34f68d130eccf5c479471fa6f9a113
sergrage <elenergetica87@mail.ru>
2024-09-20 21:53:10 +0300

    fix svg loader - 6

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -10,7 +10,7 @@ declare module "*.jpeg";
 declare module '*.svg?react' {
   import React from 'react';
 
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  const SVG: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default SVG;
 }
 
