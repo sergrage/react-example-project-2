f2d1021bdbdefae855e4ccf4653217b9719d42b7
sergrage <elenergetica87@mail.ru>
2024-09-21 10:51:57 +0300

    fix svg loader - 10

--- config/build/loaders/buildSvgLoader.ts
+++ config/build/loaders/buildSvgLoader.ts
@@ -1,6 +1,25 @@
 export function buildSvgLoader() {
     return {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      }
+      test: /\.svg?$/,
+      oneOf: [
+        {
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                prettier: false,
+                // svgo: true,
+                // svgoConfig: {
+                //   plugins: [{removeViewBox: false}],
+                // },
+                titleProp: true,
+              },
+            },
+          ],
+          issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+          },
+        },
+      ]
+  }
 }
